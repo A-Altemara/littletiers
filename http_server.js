0dbf4303667807553fce4533bcf658b6f171fcb9
Airiel Altemara <vsokord#gmail.com>
2023-08-15 13:52:05 -0700

    updated dependencies and ReadMe

--- http_server.js
+++ http_server.js
@@ -1,66 +1,30 @@
 // add http server
 // -----------------------
 // YOUR CODE
-
-var low     = require('lowdb');
-var fs      = require('lowdb/adapters/FileSync');
-var adapter = new fs('db.json');
-var db      = low(adapter);
+const express = require('express');
+const app = express();
+const low = require('lowdb');
+const fs = require('lowdb/adapters/FileSync');
+const adapter = new fs('db.json');
+const db = low(adapter);
 
 // configure express to serve static files from public directory
 // ------------------------------------------------------------------
 // YOUR CODE
+app.use(express.static('public'));
 
 // init the data store
-db.defaults({ posts: []}).write();
+db.defaults({ users: [] }).write();
 
-// list posts
-app.get('/data', function(req, res){     
-
-    // YOUR CODE
-
-});
-
-// ----------------------------------------------------
-// add post - test using:
-//      curl http://localhost:3000/posts/ping/1/false
-// ----------------------------------------------------
-app.get('/posts/:title/:id/:published', function(req, res){
-
-    // YOUR CODE
+// // list all users
+app.get('/data', function (req, res) {
+    res.send(db.get('users').value());
 
 });
 
-// ----------------------------------------------------
-// filter by published state - test using:
-//      curl http://localhost:3000/published/true
-// ----------------------------------------------------
-app.get('/published/:boolean', function(req, res){
-
-    // YOUR CODE
-
-});
-
-// ----------------------------------------------------
-// update published value - test using:
-//      curl http://localhost:3000/published/1/true
-// ----------------------------------------------------
-app.get('/published/:id/:boolean', function(req, res){
-
-    // YOUR CODE
-
-});
-
-// ----------------------------------------------------
-// delete entry by id - test using:
-//      curl http://localhost:3000/delete/5
-// ----------------------------------------------------
-app.get('/delete/:id/', function(req, res){
-
-    // YOUR CODE
-
-});
 
 // start server
 // -----------------------
-// YOUR CODE
+app.listen(3000, () => {
+    console.log(`Running on port 3000!`)
+});
