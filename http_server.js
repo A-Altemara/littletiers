9b718165e6e1362fb03d25f4adf4a011eb6e459a
Airiel Altemara <vsokord#gmail.com>
2023-08-15 16:38:45 -0700

    added body-parser and user route

--- http_server.js
+++ http_server.js
@@ -7,6 +7,10 @@ const low = require('lowdb');
 const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
 const db = low(adapter);
+// body parser has been depreciated as of 2020 but can still be useful.
+var bodyPasrser = require('body-parser');
+app.use(bodyPasrser.urlencoded({ extended: false }));
+app.use(bodyPasrser.json());
 
 // configure express to serve static files from public directory
 // ------------------------------------------------------------------
@@ -22,6 +26,32 @@ app.get('/data', function (req, res) {
 
 });
 
+// post route this uses bodyPasrser which has been depreciated as of 2020
+// test using curl -H "Content-Type: application/json" -X POST -d '{"username":"peterparker", "password":"secret"}' http://localhost:3000/test
+app.post('/test', function (req, res) {
+    console.log(req.body.username, req.body.password);
+    res.send(req.body.username + " " + req.body.password);
+})
+
+// add user
+app.post('/add', function (req, res) {
+    var user = {
+        'name': req.body.name,
+        'dob': req.body.dob,
+        'email': req.body.email,
+        'username': req.body.username,
+        'password': req.body.password,
+        'phone': req.body.phone,
+        'streetaddress': req.body.streetaddress,
+        'citystatezip': req.body.citystatezip,
+        'latitude': req.body.latitude,
+        'longitude': req.body.longitude,
+        'avatar': faker.internet.avatar()
+    }
+    db.get('users').push(user).write();
+    console.log(db.get('users').value());
+    res.send(db.get('users').value());
+});
 
 // start server
 // -----------------------
